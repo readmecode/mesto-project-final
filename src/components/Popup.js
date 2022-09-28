eaa59a376d794062c3b0f689b32ea43a91f84f58
azatgajnutdinov <azatgaynutdinov2006@yandex.ru>
2022-09-28 13:34:01 +0300

    отправка на ревью

---
+++ src/components/Popup.js
@@ -0,0 +1,32 @@
+export default class Popup {
+  constructor(selectorPopup) {
+    this._scroll = document.querySelector('.body')
+    this._popup = document.querySelector(selectorPopup);
+    this._handleEscClose = this._handleEscClose.bind(this)
+  }
+
+  open() {
+    this._popup.classList.add('popup_opened')
+    this._scroll.classList.add('body_active')
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove('popup_opened')
+    this._scroll.classList.remove('body_active')
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if(evt.key === 'Escape') {
+      this.close()
+    }
+  }
+
+  setEventListener() {
+      this._popup.addEventListener('click', evt => {
+        evt.target.classList.contains('popup__button-close') ? this.close() : false;
+    })
+    this._popup.querySelector('.popup__overlay').addEventListener('click', this.close.bind(this)); 
+  }
+}
